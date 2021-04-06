cce7a748af09811e35bf2a57f7b2984827e115b2
Sam Toll <sam.x.toll@gmail.com>
2021-04-06 11:55:43 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Samantha Toll</h1>
+        <h2>Thank you for visiting my site!</h2>
+        <h3>There is currently an edit in progress... please check back soon!</h3>
+      </header>
+    </div>
+  );
+}
+
+export default App;
